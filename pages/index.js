71cfd8a09719669b1be4d850fc3925fc8a8ef63c
Cláudio Mindouro <claudiopedrom@gmail.com>
2020-07-26 20:47:40 +0100

    feat: exit, initial, animate, stagger

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,43 @@
 import Link from 'next/link'
 import fetch from 'isomorphic-unfetch'
 import { motion } from 'framer-motion'
+import { fadeInUp, stagger } from '../animations'
+
+// animate: defines animation
+// initial: defines initial state of animation or starting point
+// exit: defines animation when component exits
 
 const Index = (props) => (
-  <motion.div exit={{ opacity: 0 }}>
+  <motion.div exit={{ opacity: 0 }} initial="initial" animate="animate">
     <div className="container center">
       <div className="title">
         <h1>Select a protein</h1>
       </div>
-      <div className="product-row">
+      <motion.div variants={stagger} className="product-row">
         {props.products.map((product) => (
           <Link key={product.id} href="/products/[id]" as={`/products/${product.id}`}>
-            <div className="card">
+            <motion.div
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              variants={fadeInUp}
+              className="card">
               <span className="category">Protein</span>
-              <img key={product.image} src={product.image} width={250} />
+              <motion.img
+                initial={{ x: 60, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ delay: 0.2 }}
+                key={product.image}
+                src={product.image}
+                width={250}
+              />
               <div className="product-info">
                 <h4>{product.name}</h4>
                 <span>{product.price}</span>
               </div>
-            </div>
+            </motion.div>
           </Link>
         ))}
-      </div>
+      </motion.div>
     </div>
   </motion.div>
 )
